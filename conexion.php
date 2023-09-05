0616b3f9c925bc05418606abcb97119479f9decf
xollari <lunoxbp@gmail.com>
2023-09-05 01:09:22 -0500

    Subida del proyecto

---
+++ conexion.php
@@ -0,0 +1,29 @@
+<?php
+  
+
+  // Data 
+  $DBhostname = 'localhost';  
+  $DBusername = 'id21118080_root2';
+  $DBpassword = ''; 
+  $DBname = 'id21118080_prueba_proyecto_uno';
+  
+
+
+  //Connection
+  $conn=mysqli_connect($DBhostname, $DBusername, $DBpassword, $DBname);
+  
+  if(!$conn){
+    die("Connection failed". mysqli_connect_error());
+    }
+    echo("Connection Succesfully");
+
+     
+  
+
+  
+  
+
+ ?>
+
+ 
+    
---
+++ js/redirigir.js
@@ -0,0 +1,7 @@
+function redEstProf(){
+    window.location.href = "registrarseE_P.html";
+}
+
+function redAdmin(){
+    window.location.href = "registrarseAdmin.html";
+}
